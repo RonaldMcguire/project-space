ee506c6a7cf620e44663997250377bd17c97a507
Ronald McGuire <ronaldmcguire@PSADMINs-MacBook-Air-86.local>
2020-06-25 14:23:22 -0400

    yes

---
+++ app.js
@@ -0,0 +1,13 @@
+// console.log("are you there?");
+// alert('oh hai!');
+// prompt("are you there???");
+
+// I would like to start with the main ship
+//this will allow me to build the ship to get ready for battle (class)
+//next will be one alien ship. this will help me set up the battle(class)
+// start setting the code for them to battle
+
+class Spaceship{
+    constructor(HP , Firepower , )
+
+}
